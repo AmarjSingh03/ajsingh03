232a39a6a2b2288bee4d4b3c1498c5f22cef632d
AmarjSingh03 <87119525+AmarjSingh03@users.noreply.github.com>
2021-07-08 14:28:30 +0530

    initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+#Add implementaton
+def add(x,y):
+return x+y
+
+#Subtract implementaton
+def subtract(x,y):
+return x-y
+
+#Multiply implementaton
+def multiply(x,y):
+return x*y
+
+#Divide implementaton
+def divide(x,y):
+return x/
+
+#implementation
+def square(x)
+pass
